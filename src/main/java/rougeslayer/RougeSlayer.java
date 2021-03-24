c53cb9a7a137c7669e14c4fbc22d0509877cc85a
chieltje2000 <bouwmanchiel@gmail.com>
2021-03-24 13:29:17 +0100

    added rougeSlayer class

---
+++ src/main/java/rougeslayer/RougeSlayer.java
@@ -0,0 +1,24 @@
+package rougeslayer;
+
+import nl.han.ica.oopg.engine.GameEngine;
+
+public class RougeSlayer extends GameEngine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
